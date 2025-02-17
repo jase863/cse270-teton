d365681243df81af728488a2a1f17430e058539a
Jason Walker <walker.jason8630@gmail.com>
2025-02-16 18:51:06 -0700

    Let's try this again

--- cse270/tests/test_smokeTest.py
+++ cse270/tests/test_smokeTest.py
@@ -9,10 +9,6 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.chrome.options import Options
-
-from selenium import webdriver
-from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 
 class TestSmokeTest():
